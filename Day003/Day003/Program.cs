6b2616b7f5d06d3b4486b6b7b3e8a53247b06b70
Robert Graves <robertjgraves@gmail.com>
2014-04-11 13:00:52 -0700

    collect input and calculate area

--- Day003/Day003/Program.cs
+++ Day003/Day003/Program.cs
@@ -10,9 +10,30 @@ namespace Day003
     {
         static void Main(string[] args)
         {
+            double length = 0;
+            double width = 0;
+            string unitOfMeasure = "";
+
+            Console.WriteLine("What unit of measure are you using?");
+            unitOfMeasure = Console.ReadLine();
+
+            Console.WriteLine("Enter the length of your rectangle or square:");
+            length = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the width of your rectangle or square:");
+            width = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("You entered {0} for the length and {1} for the width.", length, width);
+            Console.WriteLine("The area of your rectangle or square is: {0} square {1}", calculateArea(length, width), unitOfMeasure);
+
+
+            Console.ReadLine();
             
+        }
 
-
+        static double calculateArea(double l, double w)
+        {
+            return l * w;
         }
     }
 }
